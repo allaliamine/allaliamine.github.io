5c54490e005c20831182663e29c1f345104e613c
amineallali <amineallali9@gmail.com>
2024-09-16 12:30:55 +0100

    new file:   index.html 	new file:   script.js 	new file:   style.css

---
+++ script.js
@@ -0,0 +1,9 @@
+var text = "Mohamed Amin";
+
+for(var i in text) { 
+  if(text[i] === " ") {
+    $(".wavetext").append( $("<span>").html("&nbsp;") ); 
+  } else {  
+    $(".wavetext").append( $("<span>").text(text[i]) ); 
+  }
+}
